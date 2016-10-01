3e50e2766b4114a24809d5f586d2cc49b96d6b85
jerrgree <jrggb@yahoo.com>
2016-09-30 21:55:25 -0400

    Game Generation

--- MineSweepAI/main.cpp
+++ MineSweepAI/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include "MineSweeper.h"
+#include "include/MineSweeper.h"
 
 using namespace std;
 
 int main()
 {
-
+    MineSweeper ms;
+    ms.generateGame(0, 0);
     return 0;
 }
--- MineSweepAI/src/MineSweeper.cpp
+++ MineSweepAI/src/MineSweeper.cpp
@@ -1,8 +1,17 @@
-#include "MineSweeper.h"
+#include "../include/MineSweeper.h"
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <iomanip>
 
 MineSweeper::MineSweeper()
 {
-    //ctor
+    size = 16;
+}
+
+void MineSweeper::changeSize(ui newSize)
+{
+    size = newSize;
 }
 
 void MineSweeper::print()
@@ -11,22 +20,110 @@ void MineSweeper::print()
     return;
 }
 
-void generateGame()
+void MineSweeper::generateGame(ui x, ui y)
 {
-    return;
+    gameBoard.resize(size);
+    userBoard.resize(size);
+
+    for (ui i = 0; i < gameBoard.size(); i++)
+    {
+        gameBoard[i].resize(size);
+    }
+
+    for (ui i = 0; i < userBoard.size(); i++)
+    {
+        userBoard[i].resize(size);
+    }
+
+    for (ui i = 0; i < userBoard.size(); i++)
+    {
+        for (ui j = 0; j < userBoard.size(); j++)
+        {
+            userBoard[i][j] = COVERED_SPACE;
+        }
+    }
+    random_device rd;
+
+    for (ui i = 0; i < 40; i++)
+    {
+        bool validGen = false;
+        while(!validGen)
+        {
+            ui gx, gy;
+            gx = rd() % size;
+            gy = rd() % size;
+
+            if (x != gx && y != gy)
+            {
+                if (gameBoard[gx][gy] != -1)
+                {
+                    gameBoard[gx][gy] = -1;
+                    validGen = true;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < gameBoard.size(); i++)
+    {
+        for (int j = 0; j < gameBoard.size(); j++)
+        {
+            if (gameBoard[i][j] != -1)
+            {
+
+                int count = 0;
+
+                if (i - 1 > 0)
+                {
+                    if ((j - 1) > 0 && gameBoard[i - 1][j - 1] == -1)
+                        count++;
+                    if (gameBoard[i - 1][j] == -1)
+                        count++;
+                    if (j + 1 < size && gameBoard[i - 1][j + 1] == -1)
+                        count++;
+                }
+
+                if (j - 1 > 0 && gameBoard[i][j - 1] == -1)
+                    count++;
+                if (j + 1 < size && gameBoard[i][j + 1] == -1)
+                    count++;
+
+                if (i + 1 < size)
+                {
+                    if (j - 1 > 0 && gameBoard[i + 1][j - 1] == -1)
+                        count++;
+                    if (gameBoard[i + 1][j] == -1)
+                        count++;
+                    if (j + 1 < size && gameBoard[i + 1][j + 1] == -1)
+                        count++;
+                }
+
+                gameBoard[i][j] = count;
+            }
+        }
+    }
+
+    for (auto i : gameBoard)
+    {
+        for (auto j : i)
+        {
+            cout << setw(3) << j;
+        }
+        cout << endl;
+    }
 }
 
-bool uncoverSpace()
+bool MineSweeper::uncoverSpace()
 {
 
 }
 
-bool flagSpace()
+bool MineSweeper::flagSpace()
 {
 
 }
 
-void recursiveUncover()
+void MineSweeper::recursiveUncover()
 {
 
 }
